38e07efdcfb8cdb2aa9a6bd71ab28794953ddb44
Vicky-Dai <dai.ke1@northeastern.edu>
2024-11-30 15:52:05 -0800

    1130mac

---
+++ Monotone Stack/3. 503 下一个更大元素II 成环.py
@@ -0,0 +1,16 @@
+#很妙的成环处理，可以作为一个模板，以后都这么处理，通过%取模来处理
+
+class Solution:
+    def nextGreaterElements(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        result = [-1] * n
+        stack = [0]
+        for i in range(1, n*2):
+            if nums[i%n] <= nums[stack[-1]]:
+                stack.append(i%n)  #后面多的部分，如果是情况12直接压入展中，一直留在zhan里
+            else:
+                while len(stack)!=0 and nums[i%n] > nums[stack[-1]]: #多的部分如果是这个情况，会得到和前面一样的结果，所以也不怕覆盖
+                    result[stack[-1]] = nums[i%n]
+                    stack.pop()
+                stack.append(i%n)
+        return result 
