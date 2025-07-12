81dad20270ccb424baac2381317bfb0fe8ec430d
Vicky-Dai <dai.ke1@northeastern.edu>
2025-07-12 15:39:35 +0800

    improve: better graph therory and sliding window framework in array

--- Graph Theory/practice.py
+++ Graph Theory/practice.py
@@ -1,17 +1,25 @@
-def main():    
-    n, m = map(int, input().split())
-    minDist = [float('inf')] * (n + 1)
-    minDist[1] = 0  # 起点到自己的距离为0
-    edges = []
+def main():
+    n, m = map(int, input().strip().split())
+    grid = [[] for _ in range(n + 1)]
     for _ in range(m):
-        s, e, v = map(int, input().split())
-        edges.append((s, e, v))
-    for i in range(1, n):
-        for s, e, v in edges:
-            if minDist[s] != float('inf'):
-                minDist[e] = min(minDist[e], minDist[s] + v)
-    if minDist[n] == float('inf'):
-        print("unconnected")
+        s, e, v = map(int, input().strip().split())
+        grid[s].append((e, v))
+    minDist = [float('inf')] * (n + 1)
+    minDist[1] = 0
+    flag = False
+    for i in range(1, n+1):
+        if i < n:
+            for e, v in grid[i]:
+                if minDist[i] != float('inf') and minDist[e] > minDist[i] + v:
+                    minDist[e] = minDist[i] + v
+        else:
+            for e, v in grid[i]:
+                if minDist[i] != float('inf') and minDist[e] > minDist[i] + v:
+                    flag = True
+    if flag:
+        print('circle')
+    elif minDist[n] == float('inf'):
+        print('unconnected')
     else:
         print(minDist[n])
 if __name__ == "__main__":
